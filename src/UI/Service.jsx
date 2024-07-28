14f6cfd64462ad45c9e99a8a5edd85cef6607737
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-28 21:29:05 +0530

    service page 95% completed

--- src/UI/Service.jsx
+++ src/UI/Service.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper/modules";
 import { SlNote } from "react-icons/sl";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { IoCodeSlash } from "react-icons/io5";
@@ -6,14 +11,14 @@ import { BsPersonArmsUp } from "react-icons/bs";
 import { MdSlowMotionVideo } from "react-icons/md";
 import { BsFillPersonVcardFill } from "react-icons/bs";
 
-export default function Service() {
+const Activeslider = () => {
   const cards = [
     {
       logo: IoCodeSlash,
       head: "Developer",
       sub: "Innovative Web Solutions",
       content:
-        " I create dynamic, user-friendly applications. From building new apps to optimizing existing ones, I deliver high-quality code and exceptional user experiences. Available for freelance projects to turn your vision into reality. ",
+        "I create dynamic, user-friendly applications. I deliver high-quality code and exceptional user experiences. Available for freelance projects to turn your vision into reality.",
       arrow: FaArrowRightLong,
     },
     {
@@ -21,7 +26,7 @@ export default function Service() {
       head: "Designer",
       sub: "Visual Masterpieces",
       content:
-        " I transform ideas into stunning designs, specializing in graphic design, UI/UX, and branding. My designs resonate with your audience and reflect your brand's unique identity.",
+        "I transform ideas into stunning designs, specializing in graphic design, UI/UX, and branding. My designs resonate with your audience and reflect your brand's unique identity.",
       arrow: FaArrowRightLong,
     },
     {
@@ -29,7 +34,7 @@ export default function Service() {
       head: "Dancer",
       sub: "Expressive Movements",
       content:
-        " Dance is my passion, allowing me to express emotions and stories through various styles. I bring energy and creativity to every performance, captivating audiences with my art.",
+        "Dance is my passion, allowing me to express emotions and stories through various styles. I bring energy and creativity to every performance, captivating audiences with my art.",
       arrow: FaArrowRightLong,
     },
     {
@@ -37,7 +42,7 @@ export default function Service() {
       head: "Content Creator",
       sub: "Engaging Content",
       content:
-        " I produce high-quality content across platforms, including blog posts, videos, and social media reels. My goal is to inform, entertain, and inspire, helping you connect with your audience.",
+        "I produce high-quality content across platforms, including blog posts, videos, and social media reels. My goal is to inform, entertain, and inspire, helping you connect with your audience.",
       arrow: FaArrowRightLong,
     },
     {
@@ -45,7 +50,7 @@ export default function Service() {
       head: "Video Editor",
       sub: "Crafting Visual Stories",
       content:
-        " As a skilled video editor, I turn raw footage into polished, engaging videos. Specializing in storytelling, I enhance your content with seamless transitions, effects, and sound.",
+        "As a skilled video editor, I turn raw footage into polished, engaging videos. Specializing in storytelling, I enhance your content with seamless transitions, effects, and sound.",
       arrow: FaArrowRightLong,
     },
     {
@@ -53,28 +58,53 @@ export default function Service() {
       head: "Freelancer",
       sub: "Versatile and Reliable",
       content:
-        " As a freelancer, I offer a range of services including  web development, video editing. With a commitment to quality and timely delivery, Let's collaborate and achieve your goals together.",
+        "As a freelancer, I offer a range of services including web development and video editing. With a commitment to quality and timely delivery, let's collaborate and achieve your goals together.",
       arrow: FaArrowRightLong,
     },
   ];
-  return (
-    <div>
-      <h1 className="text-5xl font-bold mt-16">My Services..</h1>
 
-      <div className="mt-12 flex gap-12">
+  return (
+    <div className="flex items-center justify-center flex-col h-[900px]">
+      <h1 className="text-5xl font-bold mt-16 text-white">My Services</h1>
+      <Swiper
+        breakpoints={{
+          340: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          700: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+        }}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="max-w-[90%] lg:max-w-[80%] mt-12"
+      >
         {cards.map((card, index) => (
-          <div
-            key={index}
-            className="border-2 border-black p-5 w-72 rounded-lg hover:scale-95 space-y-4"
-          >
-            <card.logo className="w-10 h-12" />
-            <h2 className="text-2xl font-bold">{card.head}</h2>
-            <h4 className="text-[16px] font-semibold">{card.sub}</h4>
-            <p>{card.content}</p>
-            <card.arrow className="w-8 h-10" />
-          </div>
+          <SwiperSlide key={index}>
+            <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
+              <div
+                className="absolute inset-0 bg-cover bg-center"
+                style={{ backgroundImage: `url(${card.backgroundImage})` }}
+              />
+              <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
+              <div className="relative flex flex-col gap-3">
+                <card.logo className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
+                <h2 className="text-xl lg:text-2xl">{card.head}</h2>
+                <h4 className="text-[16px] font-semibold">{card.sub}</h4>
+                <p className="lg:text-[18px]">{card.content}</p>
+              </div>
+              <card.arrow className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:-rotate-45 duration-100" />
+            </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
-}
+};
+
+export default Activeslider;
