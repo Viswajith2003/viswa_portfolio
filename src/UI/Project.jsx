791ed30c7ad8dcb023bc3ed1893448c9760f236c
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-31 14:16:42 +0530

    project page wrking

--- src/UI/Project.jsx
+++ src/UI/Project.jsx
@@ -1,81 +1,111 @@
 import React from "react";
-import netflix from "../Assets/img/netflix.jpg";
-import Travel from "../Assets/img/Travel.png";
-import Fd from "../Assets/img/restuarant.png";
-import gym from "../Assets/img/gym.jpg";
-import blog from "../Assets/img/blog.png";
-import weather from "../Assets/img/weather.png";
 
-const Project = () => {
+export default function Project() {
   return (
-    <section id="Project ">
-      <div className="container">
-        <div className="flex items-center justify-center mt-20 flex-wrap">
-          <div className="mb-7 sm:mb-0 text-center">
-            <h3 className="font-bold text-6xl mt-10 w-full text-center">
-              My recent projects
-            </h3>
+    <div>
+      <div>
+        <h1>
+          Somthings <br /> I've worked on
+        </h1>
+      </div>
+      <div>
+        <div className="p-3">
+          <div>image</div>
+          <h1>Heading</h1>
+          <div className="flex gap-3">
+            <div>HTML</div>
+            <div>CSS</div>
+            <div>JS</div>
           </div>
-        </div>
-        <div className="flex items-center gap-4 flex-wrap mt-12">
-          <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-            <figure>
-              <img
-                className="rounded-[8px] border-2 border-black"
-                src={netflix}
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-            <figure>
-              <img
-                className="rounded-[8px] border-2 border-black"
-                src={Travel}
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-            <figure>
-              <img
-                className="rounded-[8px] border-2 border-black"
-                src={Fd}
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-            <figure>
-              <img
-                className="rounded-[8px] border-2 border-black"
-                src={blog}
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-            <figure>
-              <img
-                className="rounded-[8px] border-2 border-black"
-                src={weather}
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-            <figure>
-              <img
-                className="rounded-[8px] border-2 border-black"
-                src={gym}
-                alt=""
-              />
-            </figure>
+          <p>contents</p>
+          <div>
+            <div>code</div>
+            <div>Live View</div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
-};
+}
 
-export default Project;
+// import React from "react";
+// import netflix from "../Assets/img/netflix.jpg";
+// import Travel from "../Assets/img/Travel.png";
+// import Fd from "../Assets/img/restuarant.png";
+// import gym from "../Assets/img/gym.jpg";
+// import blog from "../Assets/img/blog.png";
+// import weather from "../Assets/img/weather.png";
+
+// const Project = () => {
+//   return (
+//     <section id="Project ">
+//       <div className="container">
+//         <div className="flex items-center justify-center mt-20 flex-wrap">
+//           <div className="mb-7 sm:mb-0 text-center">
+//             <h3 className="font-bold text-6xl mt-10 w-full text-center">
+//               My recent projects
+//             </h3>
+//           </div>
+//         </div>
+//         <div className="flex items-center gap-4 flex-wrap mt-12">
+//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
+//             <figure>
+//               <img
+//                 className="rounded-[8px] border-2 border-black"
+//                 src={netflix}
+//                 alt=""
+//               />
+//             </figure>
+//           </div>
+//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
+//             <figure>
+//               <img
+//                 className="rounded-[8px] border-2 border-black"
+//                 src={Travel}
+//                 alt=""
+//               />
+//             </figure>
+//           </div>
+//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
+//             <figure>
+//               <img
+//                 className="rounded-[8px] border-2 border-black"
+//                 src={Fd}
+//                 alt=""
+//               />
+//             </figure>
+//           </div>
+//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
+//             <figure>
+//               <img
+//                 className="rounded-[8px] border-2 border-black"
+//                 src={blog}
+//                 alt=""
+//               />
+//             </figure>
+//           </div>
+//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
+//             <figure>
+//               <img
+//                 className="rounded-[8px] border-2 border-black"
+//                 src={weather}
+//                 alt=""
+//               />
+//             </figure>
+//           </div>
+//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
+//             <figure>
+//               <img
+//                 className="rounded-[8px] border-2 border-black"
+//                 src={gym}
+//                 alt=""
+//               />
+//             </figure>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Project;
