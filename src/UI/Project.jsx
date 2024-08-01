9cfcff1e2b333986d0ceeeadc2b8336caaf9a5f2
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-01 13:00:23 +0530

    project page 80% set

--- src/UI/Project.jsx
+++ src/UI/Project.jsx
@@ -1,125 +1,84 @@
 import React from "react";
-import { IoCodeSlashSharp } from "react-icons/io5";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../CSS/project.css";
+
+import { data } from "../lib/projectdata";
 
 export default function Project() {
   return (
-    <div>
-      <div className="mt-14">
-        <h1 className="text-4xl font-bold">
-          Somthings <br /> I've worked on
-        </h1>
-      </div>
-      <div className="mt-16">
-        <div className="p-3 border border-black rounded-lg w-72 h-auto">
-          <div className="border border-black rounded-lg w-54 h-40"></div>
-          <h1 className="text-2xl font-bold mt-3">Heading</h1>
-          <div className="flex gap-3 my-3 text-center">
-            <div className="border border-black p-1 rounded-full w-auto h-7 text-[14px] px-2">
-              HTML
+    <div className="container mx-auto p-4">
+      <h1 className="heading text-4xl font-bold mt-14">
+        Somethings <br /> I've worked on
+      </h1>
+      <Swiper
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ clickable: true }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper-container mt-14 gap-8 bg-red-500 py-12"
+      >
+        {data.map((item, index) => (
+          <SwiperSlide
+            key={index}
+            className="card p-3 border border-black rounded-lg w-72 h-auto bg-slate-600"
+          >
+            <div className="border border-black rounded-lg w-54 h-40">
+              <img
+                src={item.img}
+                alt={item.head}
+                className="w-full h-full object-cover rounded-lg"
+              />
             </div>
-            <div className="border border-black p-1 rounded-full w-auto h-7 text-[14px] px-2">
-              CSS
+            <h1 className="text-2xl font-bold mt-3">{item.head}</h1>
+            <div className="flex gap-3 my-3 text-center">
+              <div className="border border-black p-1 rounded-full w-auto h-7 text-[14px] px-2">
+                {item.lang1}
+              </div>
+              <div className="border border-black p-1 rounded-full w-auto h-7 text-[14px] px-2">
+                {item.lang2}
+              </div>
+              <div className="border border-black p-1 rounded-full w-auto h-7 text-[14px] px-2">
+                {item.lang3}
+              </div>
             </div>
-            <div className="border border-black p-1 rounded-full w-auto h-7 text-[14px] px-2">
-              JavaScript
+            <p>{item.content}</p>
+            <div className="flex gap-3 mt-3 text-center">
+              <div className="border border-black p-2 rounded-xl w-1/4">
+                <item.icon className="w-10 h-6" />
+              </div>
+              <div className="border border-black p-2 rounded-xl w-3/4">
+                {item.btn}
+              </div>
             </div>
-          </div>
-          <p>
-            Web technologies include tools and techniques for developing
-            websites and web applications.
-          </p>
-          <div className="flex gap-3 mt-3 text-center">
-            <div className="border border-black p-2 rounded-xl w-1/4">
-              <IoCodeSlashSharp className="w-10 h-6" />
-            </div>
-            <div className="border border-black p-2 rounded-xl w-3/4">
-              Live View
-            </div>
-          </div>
+          </SwiperSlide>
+        ))}
+        <div className="swiper-button-prev slider-arrow ">
+          <ion-icon name="arrow-back-outline"></ion-icon>
         </div>
-      </div>
+        <div className="swiper-button-next slider-arrow">
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </div>
+        <div className="swiper-pagination"></div>
+      </Swiper>
     </div>
   );
 }
-
-// import React from "react";
-// import netflix from "../Assets/img/netflix.jpg";
-// import Travel from "../Assets/img/Travel.png";
-// import Fd from "../Assets/img/restuarant.png";
-// import gym from "../Assets/img/gym.jpg";
-// import blog from "../Assets/img/blog.png";
-// import weather from "../Assets/img/weather.png";
-
-// const Project = () => {
-//   return (
-//     <section id="Project ">
-//       <div className="container">
-//         <div className="flex items-center justify-center mt-20 flex-wrap">
-//           <div className="mb-7 sm:mb-0 text-center">
-//             <h3 className="font-bold text-6xl mt-10 w-full text-center">
-//               My recent projects
-//             </h3>
-//           </div>
-//         </div>
-//         <div className="flex items-center gap-4 flex-wrap mt-12">
-//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-//             <figure>
-//               <img
-//                 className="rounded-[8px] border-2 border-black"
-//                 src={netflix}
-//                 alt=""
-//               />
-//             </figure>
-//           </div>
-//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-//             <figure>
-//               <img
-//                 className="rounded-[8px] border-2 border-black"
-//                 src={Travel}
-//                 alt=""
-//               />
-//             </figure>
-//           </div>
-//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-//             <figure>
-//               <img
-//                 className="rounded-[8px] border-2 border-black"
-//                 src={Fd}
-//                 alt=""
-//               />
-//             </figure>
-//           </div>
-//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-//             <figure>
-//               <img
-//                 className="rounded-[8px] border-2 border-black"
-//                 src={blog}
-//                 alt=""
-//               />
-//             </figure>
-//           </div>
-//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-//             <figure>
-//               <img
-//                 className="rounded-[8px] border-2 border-black"
-//                 src={weather}
-//                 alt=""
-//               />
-//             </figure>
-//           </div>
-//           <div className="group max-w-full sm:w-[48.5%] md:w-[31.8%] lg: w-[32.2%] relative z-[1]">
-//             <figure>
-//               <img
-//                 className="rounded-[8px] border-2 border-black"
-//                 src={gym}
-//                 alt=""
-//               />
-//             </figure>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Project;
---
+++ src/lib/projectdata.js
@@ -0,0 +1,61 @@
+import { IoCodeSlashSharp } from "react-icons/io5";
+import blog from "../Assets/img/blog.png";
+import gym from "../Assets/img/gym.jpg";
+import netflix from "../Assets/img/netflix.jpg";
+import travel from "../Assets/img/travel.png";
+import weather from "../Assets/img/weather.png";
+
+export const data = [
+  {
+    img: blog,
+    head: "Blogging Website",
+    lang1: "HTML",
+    lang2: "CSS",
+    lang3: "JavaScript",
+    content:
+      "Web technologies include tools and techniques for developing websites and web applications.",
+    icon: IoCodeSlashSharp,
+    btn: "Live View",
+  },
+  {
+    img: gym,
+    head: "Gym Website",
+    lang1: "HTML",
+    lang2: "CSS",
+    lang3: "JavaScript",
+    content: "A platform to manage gym schedules and track workouts.",
+    icon: IoCodeSlashSharp,
+    btn: "Live View",
+  },
+  {
+    img: netflix,
+    head: "Netflix Clone",
+    lang1: "React",
+    lang2: "CSS",
+    lang3: "JavaScript",
+    content: "A clone of Netflix to stream videos online.",
+    icon: IoCodeSlashSharp,
+    btn: "Live View",
+  },
+
+  {
+    img: travel,
+    head: "Travel Website",
+    lang1: "HTML",
+    lang2: "CSS",
+    lang3: "JavaScript",
+    content: "A website to book and explore travel destinations.",
+    icon: IoCodeSlashSharp,
+    btn: "Live View",
+  },
+  {
+    img: weather,
+    head: "Weather App",
+    lang1: "React",
+    lang2: "CSS",
+    lang3: "JavaScript",
+    content: "A weather application to check current weather conditions.",
+    icon: IoCodeSlashSharp,
+    btn: "Live View",
+  },
+];
